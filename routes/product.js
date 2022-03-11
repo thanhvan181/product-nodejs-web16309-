8e90e77b876957cbe1f2d0555bd2adc650dbd855
Nguyen Thi Van <emay@Nguyens-MacBook-Air.local>
2022-03-12 00:23:29 +0700

    lan 1

---
+++ routes/product.js
@@ -0,0 +1,24 @@
+import { Router } from 'express';
+import { checkAuth } from '../middlewares/checkAuth' 
+const router = Router();
+// fake data
+const products = [
+    {id: 1, name: "Product A"},
+    {id: 2, name: "Product B"}
+];
+
+router.get('/products', checkAuth, (req, res) => {
+    res.json(products);
+});
+router.get('/product/:id', checkAuth, (req, res) => {
+    res.json(products.find(item => item.id === +req.params.id));
+});
+router.post('/products', checkAuth, (req, res) => {
+    products.push(req.body);
+    res.json(products);
+});
+router.delete('/product/:id', checkAuth, (req, res) => {
+    res.json(products.filter(item => item.id !== +req.params.id));
+});
+
+export default router;
